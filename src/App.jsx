51a3bf9abdf097350d6dc4e6e76a7743c5ea4f5e
Goggwell <samuely@frisseblikken.com>
2022-06-01 15:31:20 +0700

    breakpoints

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { Global, css, jsx } from '@emotion/react'
 import 'css-maid'
 import { variables } from './config/variables'
+import mq from './config/breakpoints'
 
 const App = () => {
     return (
@@ -17,6 +18,24 @@ const App = () => {
                             sans-serif;
                         background: ${variables.colors.darkBlue};
                         color: ${variables.colors.white};
+                    }
+
+                    ${mq[0]} {
+                        body {
+                            color: ${variables.colors.easyGrey};
+                        }
+                    }
+
+                    ${mq[1]} {
+                        body {
+                            color: ${variables.colors.middleGrey};
+                        }
+                    }
+
+                    ${mq[2]} {
+                        body {
+                            color: ${variables.colors.lightGrey};
+                        }
                     }
                 `}
             />
